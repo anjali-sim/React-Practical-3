8e70650921c0e78f1aa3de9cfd24af9ff8bb03b6
anjali-sim <anjalighetia123@gmail.com>
2023-05-02 15:27:01 +0530

    :sparkles: Created Input component along with its styled component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,13 @@
+Input.js
+import React from 'react'
+import {InputStyle} from '../../styled/InputStyle'
+
+function Input(props) {
+  return (
+    <>
+          <InputStyle type="text" value={props.value} onChange={props.onChange}></InputStyle>
+    </>
+  )
+}
+
+export default Input
---
+++ src/styled/InputStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const InputStyle = styled.input`
+
+width: "340px";
+    height: "30px";
+    border-bottom: "solid";
+    border-top: "none";
+    border-right: "none";
+    border-left: "none";
+    border-color: "lightgrey";
+`;
